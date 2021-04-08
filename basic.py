0d1a735834d9c07654944e38ab216a6919554b86
Sohaib Mubashir <sohaibmh@yahoo.com>
2021-04-08 12:30:03 +0100

    if else

--- basic.py
+++ basic.py
@@ -1,5 +1,13 @@
-def mean(mylist):
-    the_mean = sum(mylist) / len(mylist)
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
     return the_mean
 
-print(mean([1, 4, 6]))
+
+mean_list =  [2, 4, 6]
+mean_dict = {"a": 2, "b": 4, "c": 6}
+
+print(mean(mean_list))
+print(mean(mean_dict))
